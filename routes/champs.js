d419b775d7133b3036730523976344d13946da81
Hady Muhammed <hadyymohammed@gmail.com>
2024-04-11 16:53:49 +0200

    imported the logic to the endpoints files

--- routes/champs.js
+++ routes/champs.js
@@ -1,64 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const { Champ } = require("../models/champSchema");
+import { Router } from "express";
+const router = Router();
+import {
+  getChamp,
+  deleteChamp,
+  createChamp,
+  updateChamp,
+} from "../controllers/championController";
 
-router.get("/", async (req, res) => {
-  try {
-    if (req.query.id) {
-      const id = req.query.id;
-      const champ = await Champ.findById(id);
-      if (!champ) {
-        return res.status(404).send({ error: "Champion not found" });
-      }
-      return res.send({ champ });
-    }
-    const champs = await Champ.find();
-    return res.send({ champs });
-  } catch (error) {
-    return res.status(500).send({ error: "Internal Server Error" });
-  }
-});
+router.get("/", getChamp);
+router.delete("/", deleteChamp);
+router.post("/", createChamp);
+router.put("/", updateChamp);
 
-router.delete("/", async (req, res) => {
-  try {
-    const name = req.query.name;
-    const champ = await Champ.findOneAndDelete({ name });
-    if (!champ) {
-      return res.status(404).send({ error: "Champion not found" });
-    }
-    return res.send({ message: "Deleted successfully!" });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send({ error: "Internal Server Error" });
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const champ = new Champ({
-      ...req.body,
-    });
-    await champ.save();
-    return res.send({ message: "champ created successfully!" });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.put("/", async (req, res) => {
-  try {
-    const name = req.query.name;
-    const updates = req.body;
-    const champ = await Champ.findOneAndUpdate({ name }, updates, {
-      new: true,
-    });
-    if (!champ) {
-      return res.status(404).send({ error: "Champion not found" });
-    }
-    return res.send({ champ });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send({ error: "Internal Server Error" });
-  }
-});
-module.exports = router;
+export default router;
--- routes/workouts.js
+++ routes/workouts.js
@@ -1,77 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const { User } = require("../models/userSchema");
+// routes/userRoutes.js
+import { Router } from "express";
+const router = Router();
+import {
+  addWorkout,
+  getWorkout,
+  addRow,
+  updateRow,
+  deleteRow,
+} from "../controllers/workoutController";
 
-router.post("/", async (req, res) => {
-  try {
-    const { email, date } = req.body;
-    const user = await User.findOne({ email });
-    user.workouts.push({ date });
-    await user.save();
-    return res.send(user);
-  } catch (err) {
-    console.log(err);
-    return res.send({ message: err.message });
-  }
-});
+router.post("/", addWorkout);
+router.get("/", getWorkout);
+router.post("/rows", addRow);
+router.put("/rows", updateRow);
+router.delete("/rows", deleteRow);
 
-router.get("/", async (req, res) => {
-  try {
-    const { email, date } = req.query;
-    const user = await User.findOne({ email });
-    const workout = user.workouts.find((workout) => workout.date === date);
-    return res.send(workout);
-  } catch (err) {
-    console.log(err);
-    return res.send({ message: err.message });
-  }
-});
-
-router.post("/rows", async (req, res) => {
-  try {
-    const { email, date, exercise } = req.body;
-    const user = await User.findOne({ email });
-    const workout = user.workouts.find((workout) => workout.date === date);
-    workout.rows.push(exercise);
-    await user.save();
-    return res.send(user);
-  } catch (err) {
-    return res.status(404).send({ message: err.message });
-  }
-});
-
-router.put("/rows", async (req, res) => {
-  try {
-    const { email, date, id, editedExer } = req.body;
-    const user = await User.findOne({ email });
-    const workout = user.workouts.find((workout) => workout.date === date);
-    const row = workout.rows.find((row) => row._id.toString() === id);
-    row.set1 = editedExer.set1;
-    row.set2 = editedExer.set2;
-    row.set3 = editedExer.set3;
-    row.set4 = editedExer.set4;
-    row.rest = editedExer.rest;
-    row.weight = editedExer.weight;
-    row.exerciseName = editedExer.exerciseName;
-    await user.save();
-    return res.send(user);
-  } catch (err) {
-    return res.status(404).send({ message: err.message });
-  }
-});
-
-router.delete("/rows", async (req, res) => {
-  try {
-    const { email, date, id } = req.body;
-    const user = await User.findOne({ email });
-    const workout = user.workouts.find((workout) => workout.date === date);
-    const rowIndex = workout.rows.findIndex((row) => row._id.toString() === id);
-    workout.rows.splice(rowIndex, 1);
-    await user.save();
-    return res.send(user);
-  } catch (err) {
-    return res.status(404).send({ message: err.message });
-  }
-});
-
-module.exports = router;
+export default router;
